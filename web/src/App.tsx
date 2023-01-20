6dde02849f379c41e722449ed64c4312e0c5a759
Anna18921 <rafabispo53@gmail.com>
2023-01-20 10:12:51 -0300

    feat(components/Header): create Header components

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,11 @@
 import "./styles/global.css";
+import { Header } from './components/Header'
 
 const App = () => {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
       <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
-    
+        <Header/>
       </div>
     </div>
   );
